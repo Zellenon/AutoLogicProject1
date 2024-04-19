b779ac3f6a71a060c7c9506a4cbe637bc0d7909e
Rob Lorch <lorchrob@gmail.com>
2024-04-18 19:15:51 -0500

    Add most of propagate, not fully finished tho

--- sandbox.py
+++ sandbox.py
@@ -1,11 +1,12 @@
 from enum import Enum
+from collections import deque
 
 input = "p cnf 3 2\n1 2 -3 0\n-2 3 0"
 
 TruthValue = Enum('TruthValue', ['TRUE', 'FALSE', 'UNASSIGNED'])
 
 # First data structure: queue of literals to propagate
-to_propagate = []
+to_propagate = deque()
 
 # Second data structure: current assignment
 assignment = []
@@ -15,6 +16,9 @@ model = []
 
 # Fourth data structure: list of literals, each pointing to a list of clauses watching that literal
 literals_with_watching_clauses = []
+
+# Fifth data structure: set of clauses
+clauses = []
 
 # Debug printing function
 def print_global_state():
@@ -37,8 +41,12 @@ def initialize_data_structures(input):
   print("----- INITIALIZING GLOBAL STATE -----")
   print("input: ", input)
   print("number of variables: ", num_variables)
-  print("number of clauses: ", num_clauses)
-  print() 
+  print("number of clauses: ", num_clauses) 
+
+  for i in range(1, num_clauses+1):
+    clauses.append(input[i][:-1])
+
+  print("clauses: ", clauses, end="\n\n")
 
   for i in range(1, num_variables+1):
     model.append(TruthValue.UNASSIGNED)
@@ -78,7 +86,50 @@ def decide():
       assignment.append((i+1, decision_level)) 
       break
 
+# We have falsified curr_lit (watched by clause) and therefore need to update clause's
+# watched literals. We look for another literal to watch. If one exists, we swap it 
+# with curr_lit and return None. If not, we return the other watch 
+# literal that now needs to be propagated.
+def update_watched_literals(clause, curr_lit):
+  if int(clause[0]) == -1*curr_lit:
+    old_lit_index = 0 
+  else:
+    old_lit_index = 1
+
+  for i in range(2, len(clause)):
+    if (int(clause[i]) > 0 and model[int(clause[i])-1] != TruthValue.FALSE) or (int(clause[i]) < 0 and model[int(clause[i])-1] != TruthValue.TRUE):
+      clause[old_lit_index], clause[i] = clause[i], clause[old_lit_index]
+      return None
+
+  return int(clause[(old_lit_index + 1) % 2])
+
+# Apply unit propagation to completion (until we exhaust the queue of literals to propagate)
+# TODO: Make sure propagate updates the assignment and model data structures
+def propagate():
+  while to_propagate:
+    print("----- EXECUTING PROPAGATE RULE -----\n")
+    curr_lit = to_propagate.popleft()
+
+    # Update watched literals of all clauses watching the complement of curr_lit.
+    # For these clauses, we either (i) find a new literal to watch, or 
+    #                              (ii) add the other watched literal to to_propagate
+    
+    if curr_lit > 0:
+      curr_lit_comp_index = curr_lit * 2 - 1
+    else:
+      curr_lit_comp_index = curr_lit * 2 - 2
+
+    for clause_index in literals_with_watching_clauses[curr_lit_comp_index][1]: 
+      lit_to_prop = update_watched_literals(clauses[clause_index-1], curr_lit)
+      if lit_to_prop:
+        to_propagate.append(lit_to_prop)
+    print_global_state()
+
 initialize_data_structures(input)
 print_global_state()
 decide()
 print_global_state()
+propagate()
+decide()
+print_global_state()
+propagate()
