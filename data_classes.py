d8144d6b65bd8aaa0fdc3b5d9fb4cbc3b0b88f25
Zellenon <zellenon@gmail.com>
2024-04-21 13:57:06 -0500

    added more helper functions to the data classes

--- data_classes.py
+++ data_classes.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import List, Set
 
 
@@ -6,18 +7,33 @@ class Variable:
     value: bool
 
     def __init__(self, name: int, value: bool) -> None:
-        self.name = name
+        self.name = abs(name)
         self.value = value
 
+    def __eq__(self, value: object, /) -> bool:
+        if isinstance(value, Variable):
+            return self.name == value.name and self.value == value.value
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(str(self))
+
     def __lt__(self, other):
-        return abs(self.name) < abs(other.name)
+        return self.name < other.name
+
+    def __repr__(self) -> str:
+        return str(self.name * (1 if self.value else -1))
+
+    def inv(self):
+        return Variable(name=self.name, value=not self.value)
+
+    def same_var(self, other) -> bool:
+        return self.name == other.name
 
     @staticmethod
     def to_var(var: int):
         return Variable(name=abs(var), value=var > 0)
-
-    def __repr__(self) -> str:
-        return str(self.name * 1 if self.value else -1)
 
 
 class Clause:
@@ -29,34 +45,51 @@ class Clause:
     def __repr__(self) -> str:
         return "{ " + ", ".join({str(w) for w in self.vars}) + " }"
 
+    def has_var(self, var: Variable) -> bool:
+        return (var in self.vars) or (var.inv() in self.vars)
+
 
 class M:
     tracker: List[Variable | None]
 
+    def __init__(self) -> None:
+        self.tracker = []
+
     def append(self, elem: Variable | None):
         self.tracker += [elem]
+
+    def __repr__(self) -> str:
+        return str(self.tracker)
 
 
 class AppState:
     m: M
-    Delta: Set[Clause]
+    delta: list[Clause]
 
-    def __init__(self, m: M, Delta: Set[Clause]) -> None:
+    def __init__(self, m: M, delta: list[Clause]) -> None:
         self.m = m
-        self.Delta = Delta
+        self.delta = delta
 
     @classmethod
     def from_string(cls, s):
         return AppState(M(), parse_dimacs(s))
 
+    def literals(self):
+        return set(reduce(lambda x, y: x | y, [w.vars for w in self.delta]))
 
-def parse_dimacs(lines: List[str]) -> Set[Clause]:
-    Delta = set()
+    def __repr__(self) -> str:
+        return f"C: {self.delta}\nM: {self.m}"
+
+
+def parse_dimacs(lines: List[str]) -> list[Clause]:
+    Delta = []
     for line in lines:
         if line[0] == "c":
             continue
         elif "p cnf" in line:
             pass  # We don't actually need to do anything with this line, right?
         else:
-            Delta |= {Clause({Variable.to_var(int(w)) for w in line.split()})}
+            Delta += [
+                Clause({Variable.to_var(int(w)) for w in line.split() if int(w) != 0})
+            ]
     return Delta
