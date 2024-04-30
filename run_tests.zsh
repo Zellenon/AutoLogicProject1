69eed94b146ad5b0f3515e337867cf0a610b186b
Rob Lorch <lorchrob@gmail.com>
2024-04-30 11:55:44 -0500

    Hardcode test directory rather than passing as command-line arg

--- run_tests.zsh
+++ run_tests.zsh
@@ -1,16 +1,6 @@
 #!/bin/zsh
 
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <directory>"
-    exit 1
-fi
-
-directory="$1"
-
-if [[ ! -d "$directory" ]]; then
-    echo "Error: '$directory' is not a valid directory."
-    exit 1
-fi
+directory="./project1-tests"
 
 for subdirectory in "$directory"/*; do
     for file in "$subdirectory"/*; do
