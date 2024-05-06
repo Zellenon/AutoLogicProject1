836ba1dc367b0179007eb848f1c9d5a2ff451f06
Rob Lorch <lorchrob@gmail.com>
2024-05-06 10:02:50 -0500

    Add old test cases

---
+++ run_old_tests.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+directory="./project1-old-tests"
+
+for subdirectory in "$directory"/*; do
+    for file in "$subdirectory"/*; do
+        if [[ -f "$file" ]]; then
+            echo "Testing $file"
+            timeout 10 python3 main.py $file
+        fi
+    done
+done
