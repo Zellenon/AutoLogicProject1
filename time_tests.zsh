3fe3e983adfa8a982af91fe28a8bec87593d5813
Zellenon <zellenon@gmail.com>
2024-05-10 04:00:19 -0500

    removed unused code

--- time_tests.zsh
+++ time_tests.zsh
@@ -7,14 +7,3 @@ for file in "$directory"/**/**; do
     echo "Timing $file"
     time python3 main.py $file > /dev/null
 done
-
-# for subdirectory in "$directory"/*; do
-#     for file in "$subdirectory"/*; do
-#         if [[ -f "$file" ]]; then
-#             echo "Testing $file iteration 1"
-#             timeout 10 python3 main.py $file
-#             echo "Testing $file iteration 2"
-#             timeout 10 python3 main.py $file
-#         fi
-#     done
-# done
